d3d664b73e5545d2cf6f6adb6fb54e719b43aab3
alanwangsgard <wan20003@byui.edu>
2023-02-15 16:50:30 -0800

    auth0

--- server.js
+++ server.js
@@ -5,11 +5,23 @@ const app = express()
 const port = process.env.PORT || 3000
 const bodyparser = require("body-parser")
 const mongodb = require("./databases/connect")
+const { auth } = require('express-openid-connect');
+
+const config = {
+	authRequired: false,
+	auth0Logout: true,
+	secret: process.env.secret,
+	baseURL: 'http://localhost:3000',
+	clientID: process.env.clientID,
+	issuerBaseURL: process.env.issuerBaseURL
+  };
 
 app.use("/", require("./routes"))
 app.use(bodyparser.json())
 app.use("/workouts", require("./routes/workouts"))
+app.use(auth(config));
 app.use("/auth", require("./routes/auth"))
+
 
 app.get('/', (req, res) => {
 	res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
